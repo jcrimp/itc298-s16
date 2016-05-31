5c543783b79e0bbacebb314552b8324fd1dd4885
Jenny Crimp <jenny.crimp@gmail.com>
2016-05-30 17:46:15 -0700

    Copies Assignment 7 files to separate folder.

---
+++ assignment_7/index.js
@@ -0,0 +1,61 @@
+var express = require('express'); 
+var exphbs = require('express-handlebars').create({
+    defaultLayout: 'main', 
+    helpers: {
+        shortDate: function(theDate){
+            var theMonth = (theDate.getMonth()+1 < 10) ? '0' + (theDate.getMonth()+1) : (theDate.getMonth()+1);
+            var theDay = (theDate.getDate() < 10 ) ? '0' + theDate.getDate() : theDate.getDate();
+            return theDate.getFullYear() + '-' + theMonth + '-' + theDay;
+        }, 
+        prettyDate: function(theDate){
+            var theMonth = (theDate.getMonth()+1 < 10) ? '0' + (theDate.getMonth()+1) : (theDate.getMonth()+1);
+            var theDay = (theDate.getDate() < 10 ) ? '0' + theDate.getDate() : theDate.getDate();
+            return theMonth + '/' + theDay + '/' + theDate.getFullYear();
+        }
+    }
+});
+var app = express();
+
+
+app.use(express.static('public'));
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use('/api', require('cors')());
+//var html_dir = './public/';
+
+app.engine('handlebars', exphbs.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), function(){
+  console.log('The server is running ' + app.get('port') + '; Press Ctrl-C to terminate.');
+});
+
+//var sampleInsertFind = require('./test_db.js');
+//sampleInsertFind.testDB();
+
+function getCurrentYear(){
+  var today = new Date();
+  var thisYear = today.getFullYear();
+  return {year:thisYear};
+}
+
+app.use(function(req, res, next){
+        if(!res.locals.partials) res.locals.partials = {};
+        res.locals.partials.currentYear = getCurrentYear();
+        next();
+});
+
+require('./routes.js')(app);
+
+app.use(function(req, res){
+  //res.type('text/plain');
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('text/plain');
+  res.status(500);
+  res.send('500 - Server Error');
+});
---
+++ assignment_7/routes.js
@@ -0,0 +1,167 @@
+module.exports = function(app){
+    var Album = require('./models/album.js');
+    
+    app.get('/', function(req, res){
+        Album.find({}, function(err, albums){
+            if(err) throw err;
+            var context = {
+                albums: albums.map(function(album){
+                    return {
+                        id: album._id, 
+                        name: album.name,
+                        artist: album.artist,
+                        release: album.release,
+                        tracksNum: album.tracksNum,
+                        inStock: album.inStock,
+                        slug: album.slug
+                    }
+                })
+            };
+            res.render('home', context);
+        });
+        
+    });
+    
+    app.get('/about', function(req, res){
+        res.render('about');
+    });
+    
+    app.get('/album/:myAlbum', function(req, res){
+        var item = req.params.myAlbum;
+        
+        Album.findOne({slug:item}, function(err, album){
+            if(err) throw err;
+            if(album){
+                console.log(album);
+                res.render('detail', {album});
+            }
+            else{
+                res.status(404).render('404');
+            }
+        });
+    });
+    
+    app.post('/search', function(req, res){
+        var searchName = req.body.searchAlbumName;
+        res.locals.search = searchName;
+        var myPattern = new RegExp(searchName, 'i');
+        Album.findOne({name:{$regex:myPattern}}, function(err, album){
+            if(err) throw err;
+            console.log(album);
+            res.render('search', {album});
+        });
+    });
+    
+    app.post('/add', function(req, res){       
+        var albumNameToAdd = req.body.addAlbumName.toString();
+        var myPattern = new RegExp(albumNameToAdd, 'i');
+        //first, search for the album that's being added to see if it's already there
+        Album.findOne({name: {$regex:myPattern}}, function(err, album){
+            if(err) throw err;
+            //if it is already there, set add success message to false, and render the template with the existing album name
+            if(album){
+                res.locals.success = false;
+                res.render('add', {album});
+            }
+            //if it's not already there, set add success message to true, add the new album, and render the 
+            else{
+                res.locals.success = true;
+                var newAlbum = new Album({
+                    name: req.body.addAlbumName, 
+                    artist:req.body.addArtist,
+                    release: req.body.addRelease,
+                    tracksNum: req.body.addTracks,
+                    inStock: req.body.addInStock,
+                    slug: albumNameToAdd.toLowerCase().replace(" ", "-")
+                }).save(function(err, album){
+                    if(err) throw err;
+                    //console.log(album);
+                    res.render('add', {album});
+                });
+            }
+        })
+    });
+    
+    app.post('/update', function(req, res){
+        //check if body has id
+        if(req.body.id){
+            //if body has id, make sure addAlbumName doesn't already exist in a document other than the current one.
+            var albumNameToAdd = req.body.addAlbumName.toString();
+            var myPattern = new RegExp(albumNameToAdd, 'i');
+            Album.findOne({_id: {$ne:req.body.id}, name: {$regex:myPattern}}, function(err, album){
+                if(err) throw err;
+                if(album){
+                    //if it is already there, set alreadyExists to true, and render the template with the old album name
+                    res.locals.alreadyExists = true;
+                    Album.findOne({_id:req.body.id}, function(err, album){
+                        if(err) throw err;
+                        res.render('detail', {album});
+                    }); 
+                }
+                else{
+                //if addAlbumName doesn't already exist, set update success message to true, update the document with that id, and render detail page with the updated document.
+                res.locals.updateSuccess = true;
+                Album.findOne({_id:req.body.id}, function(err, album){
+                    if(err) throw err;
+                    album.name = req.body.addAlbumName; 
+                    album.artist = req.body.addArtist;
+                    album.release =  req.body.addRelease;
+                    album.tracksNum = req.body.addTracks;
+                    album.inStock = req.body.addInStock;
+                    album.slug = albumNameToAdd.toLowerCase().replace(" ", "-");
+                    album.save(function(err){
+                        if(err) throw err;
+                        console.log(album);
+                        res.render('detail', {album});
+                    });
+                });
+            }
+        });
+        }
+        else{
+            //if body doesn't have ID, you shouldn't have even gotten this far. 404 error.
+            res.type('text/plain');
+            res.status(404).send('404 - There was a problem updating the data');
+        } 
+    });
+    
+    app.post('/delete', function(req, res){
+        Album.findById(req.body.deleteId, function(err, album){
+            if(err) throw err;
+            album.remove(function(err){
+                if(err) throw err;
+                res.locals.deleteSuccess = true;
+                res.locals.deletedAlbum = album.name;
+                res.render('delete');
+            });
+        });
+    });
+    
+    
+    // API ROUTES
+    app.get('/api/albums', function(req, res){       
+        Album.find({}, function(err, albums){
+            if(err) throw err;
+            if(albums){
+                res.json(albums);
+            }
+            else{
+                res.status(404).send('404 - No Data');
+            }
+        });
+    });
+    
+    app.get('/api/album/:anAlbum', function(req, res){ 
+        Album.findOne({slug:req.params.anAlbum}, function(err, album){
+            if(err) throw err;
+            if(album){
+                res.json(album);
+            }
+            else{
+                res.type('text/plain');
+                res.status(404).send('404 - Page not found');
+            }
+        });
+    });
+
+};
